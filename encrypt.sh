8aee1587b8a3c19e70344f7b5c25505d318d577a
aberforth4 <155059882+aberforth4@users.noreply.github.com>
2025-04-08 17:51:09 +0400

    Create encrypt.sh

---
+++ encrypt.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+FILE="terraform.tfstate"
+DIR="$(dirname "$(realpath "$0")")"
+
+if [ -f "$DIR/$FILE" ]; then
+    echo "$FILE found, start encryption..."
+
+    if [ -z "$ENCRYPTION_KEY" ]; then
+        echo "ENCRYPTION_KEY variable does not set."
+        exit 1
+    fi
+
+    openssl enc -aes-256-cbc -in "$DIR/$FILE" -out "$DIR/$FILE.dec" -pass "env:ENCRYPTION_KEY"
+
+    if [ $? -eq 0 ]; then
+        echo "$FILE encrypted"
+    else
+        echo "Encryption error occured"
+        exit 1
+    fi
+else
+    echo "$FILE does not found in $DIR"
+    exit 1
+fi
