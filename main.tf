c83d238b22e97f5227b6221eac05d1c55c5590df
aberforth4 <155059882+aberforth4@users.noreply.github.com>
2025-04-09 19:10:04 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,48 +4,10 @@ import {
   id = each.value
 }
 
-
-resource "github_branch_protection" "main_protection" {
-    for_each = toset(var.repositories)
-    repository_id = each.key
-    allows_deletions                = false
-    allows_force_pushes             = false
-    enforce_admins                  = true
-    force_push_bypassers            = []
-    lock_branch                     = false
-    pattern                         = data.github_repository.repo_info[each.key].default_branch
-    require_conversation_resolution = true
-    require_signed_commits          = true
-    required_linear_history         = false
-
-    required_pull_request_reviews {
-        dismiss_stale_reviews           = true
-        dismissal_restrictions          = []
-        pull_request_bypassers          = []
-        require_code_owner_reviews      = true
-        require_last_push_approval      = false
-        required_approving_review_count = 1
-        restrict_dismissals             = false
-    }
-
-    restrict_pushes {
-        blocks_creations = true
-        push_allowances  = []
-    }
-}
-
-resource "github_repository_collaborator" "a_repo_collaborator" {
-  for_each = zipmap(var.repositories, var.collaborators)
-  username = each.value
-  permission = "push"
-  repository = each.key
-}
-
-resource "github_team_repository" "some_team_repo" {
-  for_each = zipmap(var.repositories, var.teams)
-  team_id    = each.value
-  repository = each.key
-  permission = "push"
+import {
+  for_each = var.repositories
+  to = github_branch_protection.main_protection[each.key]
+  id = "${each.value}:${data.github_repository.repo_info[each.value].default_branch}"
 }
 
 resource "github_repository" "repo" {
@@ -99,3 +61,49 @@ resource "github_repository" "repo" {
     ]
   }
 }
+
+
+resource "github_branch_protection" "main_protection" {
+    count = length(var.repositories)
+    repository_id = var.repositories[count.index]
+    allows_deletions                = false
+    allows_force_pushes             = false
+    enforce_admins                  = true
+    force_push_bypassers            = []
+    lock_branch                     = false
+    pattern                         = data.github_repository.repo_info[var.repositories[count.index]].default_branch
+    require_conversation_resolution = true
+    require_signed_commits          = true
+    required_linear_history         = false
+
+    required_pull_request_reviews {
+        dismiss_stale_reviews           = true
+        dismissal_restrictions          = []
+        pull_request_bypassers          = []
+        require_code_owner_reviews      = true
+        require_last_push_approval      = false
+        required_approving_review_count = 1
+        restrict_dismissals             = false
+    }
+
+    restrict_pushes {
+        blocks_creations = true
+        push_allowances  = []
+    }
+}
+
+
+resource "github_repository_collaborator" "a_repo_collaborator" {
+  for_each = zipmap(var.repositories, var.collaborators)
+  username = each.value
+  permission = "push"
+  repository = each.key
+}
+
+resource "github_team_repository" "some_team_repo" {
+  for_each = zipmap(var.repositories, var.teams)
+  team_id    = each.value
+  repository = each.key
+  permission = "push"
+}
+
