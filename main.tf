77282d2b0dc14cde448fdd233a1b3bab789c8c5e
aberforth4 <155059882+aberforth4@users.noreply.github.com>
2025-04-10 19:02:32 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,35 @@ import {
   id = each.value
 }
 
+locals {
+  default_branch_protections = [
+    for repo in var.repositories : {
+      repo_name    = repo
+      default_branch = data.github_repository.repo_info[repo].default_branch
+      repo_id      = data.github_branch_protection_rules.branch_protection_rules[repo].id
+      node_id      = data.github_repository.repo_info[repo].node_id
+      has_existing_protection = contains(
+        [for rule in data.github_branch_protection_rules.branch_protection_rules[repo].rules : rule.pattern],
+        data.github_repository.repo_info[repo].default_branch
+      )
+    }
+  ]
+  default_branch_protection_map = {
+    for item in local.default_branch_protections :
+    "${item.repo_name}-${item.default_branch}" => item
+  }
+}
+
+
+import {
+  for_each = {
+    for key, value in local.default_branch_protection_map :
+    key => value if value.has_existing_protection
+  }
+  to = github_branch_protection.main_protection[each.key]
+  id = "${each.value.repo_id}:${each.value.default_branch}"
+}
+
 resource "github_repository" "repo" {
   count = length(var.repositories)
   name        = var.repositories[count.index]
@@ -58,14 +87,14 @@ resource "github_repository" "repo" {
 
 
 resource "github_branch_protection" "main_protection" {
-    count = length(var.repositories)
-    repository_id = data.github_repository.repo_info[var.repositories[count.index]].node_id
+    for_each = local.default_branch_protection_map
+    repository_id = each.value.node_id
     allows_deletions                = false
     allows_force_pushes             = false
     enforce_admins                  = true
     force_push_bypassers            = []
     lock_branch                     = false
-    pattern                         = data.github_repository.repo_info[var.repositories[count.index]].default_branch
+    pattern       = each.value.default_branch
     require_conversation_resolution = true
     require_signed_commits          = true
 
@@ -83,17 +112,16 @@ resource "github_branch_protection" "main_protection" {
         blocks_creations = true
         push_allowances  = []
     }
-lifecycle {
-  ignore_changes = [
-	required_linear_history
-]
-}
 
+    lifecycle {
+        ignore_changes = [
+	required_linear_history
+       ]
+    }
 }
 
 resource "github_repository_collaborators" "repo_collaborators" {
   for_each = toset(var.repositories)
-
   repository = each.value
 
   dynamic "user" {
