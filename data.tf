afe8e13aaa892337301200528380f258f619585a
aberforth4 <155059882+aberforth4@users.noreply.github.com>
2025-04-10 09:55:30 +0400

    Update data.tf

--- data.tf
+++ data.tf
@@ -2,3 +2,8 @@ data "github_repository" "repo_info" {
   for_each = toset(var.repositories)
   name     = each.key
 }
+
+data "github_team" "teams" {
+  for_each = { for team in var.teams : team.team_id => team }
+  slug     = each.key
+}
