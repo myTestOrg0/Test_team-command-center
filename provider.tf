dc9a1b3b22f0272c0b0d2422651543a4c832238a
aberforth4 <155059882+aberforth4@users.noreply.github.com>
2025-04-11 17:36:54 +0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ provider "github" {
   app_auth {
     id              = "1201094"  
     installation_id = "63770249" 
-    pem_file        = base64decode(var.GH_APP_TOKEN)
+    pem_file        = var.GH_APP_TOKEN
   }
   owner = "myTestOrg0" 
 }
