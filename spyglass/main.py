659f0f55c8c2bbe3b36c0f14884c301053a07058
aberforth4 <155059882+aberforth4@users.noreply.github.com>
2025-04-17 16:51:58 +0400

    Update main.py

--- spyglass/main.py
+++ spyglass/main.py
@@ -6,20 +6,18 @@ from compliance_checker import ComplianceChecker
 from compl_status import ComplianceStatus
 from prettytable import PrettyTable
 from datetime import datetime, timezone
-
-
 if __name__ == '__main__':
     utc_dt = datetime.now(timezone.utc)
     token = gh_token_helper.get_access_token()
     api_helper = ApiHelper(token)
     compliance_checker = ComplianceChecker()
     table = PrettyTable()
-    table.field_names = ["Repository", "Compliance status", "Comments"]
+    table.field_names = ["Repository", "Compliance status", "To fix", "Comments"]
     for repo_name in repositories:
         repository = GHRepository(repo_name)
         repository.initialize(api_helper)
         status = compliance_checker.check_repo_compl(repository)
-        table.add_row([status.repository_name, status.status, "\n".join(status.comments)])
-        table.add_row(["-" * 5, "-" * 5, "-" * 15])
+        table.add_row([status.repository_name, status.status, "\n".join(status.problems_2_fix), "\n".join(status.comments)])
+        table.add_row(["-" * 5, "-" * 5, "-" * 15, "-" * 15])
     print(f"Date: {utc_dt.strftime("%d-%m-%Y")} Time UTC: {utc_dt.strftime("%H:%M")}")
     print(table)
