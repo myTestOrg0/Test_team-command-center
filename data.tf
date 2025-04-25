8c30f7afacafd19609af4b457d0e2cf0374bff6b
aberforth4 <155059882+aberforth4@users.noreply.github.com>
2025-04-25 18:12:38 +0400

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,14 +1,14 @@
 data "github_repository" "repo_info" {
-  for_each = toset(var.repositories)
+  for_each = local.unique_repositories
   name     = each.key
 }
 
-data "github_team" "teams" {
-  for_each = { for team in var.teams : team.team_id => team }
-  slug     = each.key
+data "github_branch_protection_rules" "branch_protection_rules" {
+  for_each = local.unique_repositories
+  repository = each.key
 }
 
-data "github_branch_protection_rules" "branch_protection_rules" {
-  for_each = toset(var.repositories)
-  repository = data.github_repository.repo_info[each.key].id  
+data "github_team" "team_id" {
+  for_each = local.team_list_set
+  slug     = each.key
 }
