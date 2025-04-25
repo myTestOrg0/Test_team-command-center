a61f6ddf6be6e1c956e533f925cda3ab9c5e8907
aberforth4 <155059882+aberforth4@users.noreply.github.com>
2025-04-25 18:12:09 +0400

    Create locals.tf

---
+++ locals.tf
@@ -0,0 +1,110 @@
+locals {
+  unique_repositories = toset([
+    for item in var.branch_protection : item.repo_name
+  ])
+
+  protected_branches = flatten([
+    for repo_key, repo_val in data.github_branch_protection_rules.branch_protection_rules : [
+      for rule in repo_val.rules : {
+        repo_name   = repo_val.repository
+        branch_name = rule.pattern
+      }
+    ]
+  ])
+
+  branches_to_import = [
+    for item in var.branch_protection : item
+    if contains([
+      for protected in local.protected_branches : "${protected.repo_name}:${protected.branch_name}"
+    ], "${item.repo_name}:${item.branch_name}")
+  ]
+
+  branches_to_import_map = {
+    for item in local.branches_to_import :
+    "${item.repo_name}:${item.branch_name}" => item
+  }
+
+  team_list_set = toset(var.team_list)
+
+ standart_branch_protection_rules = {
+    for item in var.branch_protection :
+    "${item.repo_name}:${item.branch_name}" => item
+    if item.protection_type == "standart"
+  }
+
+  custom_branch_protection_rules = {
+    for item in var.branch_protection :
+    "${item.repo_name}:${item.branch_name}" => item
+    if item.protection_type == "custom"
+  }
+
+default_branches = {
+    for repo_name in var.repositories :
+    repo_name => data.github_repository.repo_info[repo_name].default_branch
+  }
+
+  standart_branches_to_import_map = {
+    for repo_name, default_branch in local.default_branches :
+    "${repo_name}:${default_branch}" => {
+      repo_name   = repo_name
+      branch_name = default_branch
+      protection_type = "standart"
+    }
+    if contains([for rule in data.github_branch_protection_rules.branch_protection_rules[repo_name].rules : rule.pattern], default_branch)
+  }
+
+  standart_branches_to_import_map_combined = merge(
+    local.standart_branches_to_import_map,
+    {
+      for item in var.branch_protection :
+      "${item.repo_name}:${item.branch_name}" => item
+      if item.protection_type == "standart" &&
+        contains([
+          for protected in local.protected_branches : "${protected.repo_name}:${protected.branch_name}"
+        ], "${item.repo_name}:${item.branch_name}")
+    }
+  )
+
+  custom_branches_to_import_map = {
+    for item in var.branch_protection :
+    "${item.repo_name}:${item.branch_name}" => item
+    if item.protection_type == "custom" &&
+      contains([
+        for protected in local.protected_branches : "${protected.repo_name}:${protected.branch_name}"
+      ], "${item.repo_name}:${item.branch_name}")
+  }
+
+  repositories_from_collaborators_and_teams = toset(concat(
+      flatten([for c in var.collaborators : c.repository]),
+      flatten([for t in var.teams : t.repository])
+    ))
+
+  collaborators_by_repo = {
+    for repo in local.repositories_from_collaborators_and_teams :
+    repo => [
+      for user in var.collaborators : {
+        username   = user.username
+        permission = user.permission
+      }
+      if contains(user.repository, repo)
+    ]
+  }
+
+  teams_by_repo = {
+    for repo in local.repositories_from_collaborators_and_teams :
+    repo => [
+      for team in var.teams : {
+        team_id    = team.team_id
+        permission = team.permission
+      }
+      if contains(team.repository, repo)
+    ]
+  }
+
+  combined_collaborators = {
+    for repo in local.repositories_from_collaborators_and_teams : repo => {
+      users = lookup(local.collaborators_by_repo, repo, [])
+      teams = lookup(local.teams_by_repo, repo, [])
+    }
+  }
+}
