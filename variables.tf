30f8201bd2fcd8e68544238864a884df86f2f8a5
aberforth4 <155059882+aberforth4@users.noreply.github.com>
2025-04-29 17:13:35 +0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,25 +7,44 @@ variable "repositories" {
   default = ["NewRepoFor", "TestRepo"]
 }
 
+variable "team_list" {
+  description = "List of github teams"
+  default = [
+    "test_team",       
+  ]
+}
+
 variable "branch_protection" {
-  description = "List of repositories which need a protection"
+  description = "List of settings for branch protection"
   type = list(object({
     repo_name = string,
     branch_name = string,
     push_restrictions = string,
-    protection_type = string
+    protection_type = string,
+    required_approving_review_count = number
   }))
   default = [
     {
-      repo_name = "NewRepoFor", branch_name = "main", push_restrictions = "test_team", protection_type = "standart"
+      repo_name = "NewRepoFor", branch_name = "main", push_restrictions = "test_team", protection_type = "standart", "required_approving_review_count" = 4
     },
     {
-      repo_name = "NewRepoFor", branch_name = "develop", push_restrictions = "test_team", protection_type = "custom"
+      repo_name = "NewRepoFor", branch_name = "develop", push_restrictions = "test_team", protection_type = "custom", "required_approving_review_count" = 4
     },
     {
-      repo_name = "TestRepo", branch_name = "main", push_restrictions = "test_team", protection_type = "standart"
+      repo_name = "TestRepo", branch_name = "main", push_restrictions = "test_team", protection_type = "standart", "required_approving_review_count" = 4
     }
   ]
+}
+
+variable "default_branch_protection_settings" {
+  description = "Settings for default branch protection rules"
+  type = object({
+    push_restrictions = string,
+    required_approving_review_count = number
+  })
+  default = {
+    push_restrictions = "test_team", "required_approving_review_count" = 4
+  } 
 }
 
 variable "collaborators" {
@@ -35,7 +54,7 @@ variable "collaborators" {
     repository = list(string)
   }))
   default = [
-    { username = "dumnarix", permission = "admin", repository = ["NewRepoFor"]},
+    { username = "dumnarix", permission = "admin", repository = ["NewRepoFor", "TestRepo"]},
   ]
 }
 
@@ -49,14 +68,7 @@ variable "teams" {
     {
       team_id    = "test_team"
       permission = "push"
-      repository = ["NewRepoFor"]
+      repository = ["NewRepoFor", "TestRepo"]
     },
-  ]
-}
-
-variable "team_list" {
-  description = "List of github teams"
-  default = [
-    "test_team",       
   ]
 }
