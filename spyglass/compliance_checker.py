4acb2c60cdf70f2c8919d5991c45c725636fdc58
aberforth4 <155059882+aberforth4@users.noreply.github.com>
2025-05-01 18:51:38 +0400

    Update compliance_checker.py

--- spyglass/compliance_checker.py
+++ spyglass/compliance_checker.py
@@ -1,7 +1,7 @@
 from gh_branch_protection_rule import GHBranchProtectionRule
 from compl_status import ComplianceStatus
 from gh_repository import GHRepository
-from gh_user import GHUser
+import json
 
 
 class ComplianceChecker:
@@ -9,32 +9,8 @@ class ComplianceChecker:
 
     def __init__(self):
         self.compl_status = ComplianceStatus()
-
-    standard_repository = {
-        "is_secrets_scanning_enable": (True, "Secrets scanning is turned off."),
-        "is_secret_scanning_push_protection_enabled": (True, "Secrets scanning push protection is turned off."),
-        "is_dependabot_enabled": (True, "Dependabot scanning is turned off."),
-        "dependabot_open_alerts_number": (0, "Open Dependabot alerts found."),
-        "secret_scan_alerts_number": (0, "Open secrets scanning found."),
-        "have_codeowners_file": (True, "No CODEOWNERS file"),
-    }
-
-    standard_protection_rule = {
-        "is_enabled": (True, "Branch is unprotected."),
-        "dismiss_stale_reviews": (True, "Stale pull request approval is turned off."),
-        "require_code_owner_reviews": (True, "CODEOWNERS pull request review is turned off."),
-        "require_last_push_approval": (True, "Approval of the most recent reviewable push is turned off."),
-        "required_approving_review_count": (1, "Pull request approval is turned off."),
-        "required_signatures": (True, "Commit signatures are not required."),
-        "enforce_admins": (True, "Admins can bypass branch protection."),
-        "allow_force_pushes": (False, "Force pushes are turned on."),
-        "allow_deletions": (False, "Users with push access can delete branch."),
-        "required_conversation_resolution": (
-        True, "Conversation resolution is not required for merge into default branch."),
-        "required_pr": (True, "Pull request does not required before merging."),
-        "push_restrictions": (True, "No push restrictions."),
-        "bypass_pull_request_allowances": ({}, "can bypass branch protection rule.")
-    }
+        with open("check_configuration.json", "r") as f:
+            self.check_configuration = json.load(f)
 
     def check_repo_compl(self, repo: GHRepository) -> ComplianceStatus:
         """Check repository for policy compliance"""
@@ -77,44 +53,32 @@ class ComplianceChecker:
     def check_repo(self, repo: GHRepository) -> None:
         """Check repository properties for policy compliance"""
         for name, value in vars(repo).items():
-            if name in self.standard_repository.keys():
-                match name:
-                    case "is_secrets_scanning_enable":
-                        if repo.visibility != "public":
-                            continue
-                        else:
-                            if value != self.standard_repository[name][0]:
-                                self.compl_status.problems_2_fix.append(self.standard_repository[name][1])
-                    case _:
-                        if value != self.standard_repository[name][0]:
-                            self.compl_status.problems_2_fix.append(self.standard_repository[name][1])
-
-    def check_branch_protection(self, branch_protection_rule: GHBranchProtectionRule) -> None:
+            if name in self.check_configuration["repository"].keys():
+                if value != self.check_configuration["repository"][name]["st_value"]:
+                    self.compl_status.problems_2_fix.append(self.check_configuration["repository"][name]["error_msg"])
+    def check_branch_protection(self, protection_rule: GHBranchProtectionRule) -> None:
         """Check repository default branch protection for policy compliance"""
-        for name, value in vars(branch_protection_rule).items():
-            if name in self.standard_protection_rule.keys():
+        branch_name = protection_rule.branch_name
+        for name, value in vars(protection_rule).items():
+            if name in self.check_configuration["protection_rule"].keys():
+                error_msg = self.check_configuration["protection_rule"][name]["error_msg"]
+                st_value = self.check_configuration["protection_rule"][name]["st_value"]
                 match name:
                     case "is_enabled":
-                        if value != self.standard_protection_rule[name][0]:
-                            self.compl_status.problems_2_fix.append(
-                                f"[BRANCH {branch_protection_rule.branch_name}] {self.standard_protection_rule[name][1]}")
+                        if value != st_value:
+                            self.compl_status.problems_2_fix.append(f"[BRANCH {branch_name}] {error_msg}")
                             return
                     case "required_approving_review_count":
-                        if value < self.standard_protection_rule[name][0]:
-                            self.compl_status.problems_2_fix.append(
-                                f"[BRANCH {branch_protection_rule.branch_name}] {self.standard_protection_rule[name][1]}")
+                        if value < st_value:
+                            self.compl_status.problems_2_fix.append(f"[BRANCH {branch_name}] {error_msg}")
                     case "bypass_pull_request_allowances":
-                        if value != self.standard_protection_rule[name][0]:
-                            for user in branch_protection_rule.bypass_pull_request_allowances["users"]:
-                                self.compl_status.problems_2_fix.append(
-                                    f"[BRANCH {branch_protection_rule.branch_name}] User {user["login"]} {self.standard_protection_rule[name][1]}")
-                            for team in branch_protection_rule.bypass_pull_request_allowances["teams"]:
-                                self.compl_status.problems_2_fix.append(
-                                    f"[BRANCH {branch_protection_rule.branch_name}] Team {team["name"]} {self.standard_protection_rule[name][1]}")
-                            for app in branch_protection_rule.bypass_pull_request_allowances["apps"]:
-                                self.compl_status.problems_2_fix.append(
-                                    f"[BRANCH {branch_protection_rule.branch_name}] GitHub app {app["name"]} {self.standard_protection_rule[name][1]}")
+                        if value != st_value:
+                            for user in protection_rule.bypass_pull_request_allowances["users"]:
+                                self.compl_status.problems_2_fix.append(f"[BRANCH {branch_name}] User {user["login"]} {error_msg}")
+                            for team in protection_rule.bypass_pull_request_allowances["teams"]:
+                                self.compl_status.problems_2_fix.append(f"[BRANCH {branch_name}] Team {team["name"]} {error_msg}")
+                            for app in protection_rule.bypass_pull_request_allowances["apps"]:
+                                self.compl_status.problems_2_fix.append(f"[BRANCH {branch_name}] GitHub app {app["name"]} {error_msg}")
                     case _:
-                        if value != self.standard_protection_rule[name][0]:
-                            self.compl_status.problems_2_fix.append(
-                                f"[BRANCH {branch_protection_rule.branch_name}] {self.standard_protection_rule[name][1]}")
+                        if value != st_value:
+                            self.compl_status.problems_2_fix.append(f"[BRANCH {branch_name}] {error_msg}")
