2b3057dd681397e517adc746ed0eb7971893a8ba
aberforth4 <155059882+aberforth4@users.noreply.github.com>
2025-05-12 13:34:34 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,102 +1,137 @@
+################ IMPORT BLOCKS. DO NOT CHANGE! #####################
 import {
   for_each = local.standart_branches_to_import_map
-
-  to = github_branch_protection.standart_protection[each.key]
+  to = github_branch_protection.high_protection[each.key]
   id = "${each.value.repo_name}:${each.value.branch_name}"
 }
+
 import {
   for_each = local.custom_branches_to_import_map
-
-  to = github_branch_protection.custom_protection[each.key]
+  to = github_branch_protection.moderate_protection[each.key]
   id = "${each.value.repo_name}:${each.value.branch_name}"
 }
 
 import {
   for_each = local.default_branch_protection_rules_to_import
-
   to = github_branch_protection.default_branch_protection[each.key]
   id = "${each.key}:${each.value}"
 }
+####################################################################
 
+#################### BRANCH PROTECTION FOR DEFAULT BRANCH ##########
 resource "github_branch_protection" "default_branch_protection" {
-  depends_on = [github_repository_collaborators.repo_collaborators]
-  for_each = local.default_branch_protection_rules
-  repository_id = data.github_repository.repo_info[each.key].node_id
-  pattern       = each.value 
-  allows_deletions = false
-  allows_force_pushes = false
-  enforce_admins = true
-  force_push_bypassers = []
-  lock_branch = false
-  require_conversation_resolution = true
-  require_signed_commits = true
-  required_linear_history  = false
+  depends_on = [github_repository_collaborators.repo_collaborators]  # DO NOT CHANGE!
+  for_each = local.default_branch_protection_rules                   # DO NOT CHANGE!
+  repository_id = data.github_repository.repo_info[each.key].node_id # DO NOT CHANGE!
+  pattern       = each.value                                         # DO NOT CHANGE!
+  allows_deletions = false # allow users with write permission to delete branch
+  allows_force_pushes = false # allow force pushes to the branch
+  enforce_admins = true # do not allow repository admins to bypass this rule
+  force_push_bypassers = [] # list of collaborators who can bypass force push restriction
+  lock_branch = false # make branch read only
+  require_conversation_resolution = true # all conversations on code must be resolved before a pull request can be merged
+  require_signed_commits = true  # commits pushed to matching branches must have verified signatures
+  required_linear_history  = false # prevent merge commits from being pushed
   required_pull_request_reviews {
-    dismiss_stale_reviews  = true
-    dismissal_restrictions = []
-    pull_request_bypassers = []
-    require_code_owner_reviews = true
-    require_last_push_approval = true
+    dismiss_stale_reviews  = true # new reviewable commits pushed to a matching branch will dismiss pull request review approvals
+    dismissal_restrictions = [] # teams/users/apps allowed to dismiss pull request reviews 
+    pull_request_bypassers = [] # teams/users/apps who are allowed to bypass required pull requests
+    require_code_owner_reviews = true # require an approved review in pull requests including files with a designated code owner
+    require_last_push_approval = true # whether the most recent reviewable push must be approved by someone other than the person who pushed it
+    # number of approvals needed DO NOT CHANGE!
     required_approving_review_count = lookup(var.default_branch_protection_settings, "required_approving_review_count", 1)
-    restrict_dismissals = false
   } 
   restrict_pushes {
-  blocks_creations = true
+  blocks_creations = true # only people, teams, or apps allowed to push will be able to create new branches matching this rule
+  # teams that can push to these branch. DO NOT CHANGE 
   push_allowances = var.default_branch_protection_settings.push_restrictions == "" ? [] : [data.github_team.team_id[var.default_branch_protection_settings.push_restrictions].node_id]
   }
 }
+#####################################################################
 
-resource "github_branch_protection" "standart_protection" {
-  depends_on = [github_repository_collaborators.repo_collaborators]
-  for_each = local.standart_branch_protection_rules
-  repository_id = data.github_repository.repo_info[each.value.repo_name].node_id
-  pattern       = each.value.branch_name
-  allows_deletions = false
-  allows_force_pushes = false
-  enforce_admins = true
-  force_push_bypassers = []
-  lock_branch = false
-  require_conversation_resolution = true
-  require_signed_commits = true
-  required_linear_history  = false
+#################### HIGH LEVEL BRANCH PROTECTION RULE ##############
+resource "github_branch_protection" "high_protection" {
+  depends_on = [github_repository_collaborators.repo_collaborators] # DO NOT CHANGE!
+  for_each = local.standart_branch_protection_rules                 # DO NOT CHANGE!
+  repository_id = data.github_repository.repo_info[each.value.repo_name].node_id # DO NOT CHANGE!
+  pattern       = each.value.branch_name # DO NOT CHANGE!
+  allows_deletions = false # allow users with write permission to delete branch
+  allows_force_pushes = false # allow force pushes to the branch
+  enforce_admins = true # do not allow repository admins to bypass this rule
+  force_push_bypassers = [] # list of collaborators who can bypass force push restriction
+  lock_branch = false # make branch read only
+  require_conversation_resolution = true # all conversations on code must be resolved before a pull request can be merged
+  require_signed_commits = true # commits pushed to matching branches must have verified signatures
+  required_linear_history  = false # prevent merge commits from being pushed
   required_pull_request_reviews {
-    dismiss_stale_reviews  = true
-    dismissal_restrictions = []
-    pull_request_bypassers = []
-    require_code_owner_reviews = true
-    require_last_push_approval = true
+    dismiss_stale_reviews  = true # new reviewable commits pushed to a matching branch will dismiss pull request review approvals
+    pull_request_bypassers = [] # teams/users/apps who are allowed to bypass required pull requests
+    require_code_owner_reviews = true # require an approved review in pull requests including files with a designated code owner
+    require_last_push_approval = true # whether the most recent reviewable push must be approved by someone other than the person who pushed it
+    # number of approvals needed DO NOT CHANGE!
     required_approving_review_count = lookup(each.value, "required_approving_review_count", 1)
-    restrict_dismissals = false
+    restrict_dismissals = true 
+    # teams/users/apps allowed to dismiss pull request reviews DO NOT CHANGE!
+    dismissal_restrictions = each.value.review_dismissals == "" ? [] : [data.github_team.team_id[each.value.review_dismissals].node_id]
   } 
+
   restrict_pushes {
-  blocks_creations = true
+  blocks_creations = true # only people, teams, or apps allowed to push will be able to create new branches matching this rule
+  # teams that can push to these branch. DO NOT CHANGE 
   push_allowances = each.value.push_restrictions == "" ? [] : [data.github_team.team_id[each.value.push_restrictions].node_id]
   }
+
+  required_status_checks {
+    # list of workflow jobs that must be executed with exit code 0 before merging
+    # DANGER OPTION! YOUR REPOSITORY MUST HAVE SUCH JOB, OTHERWISE ALL PR TO THIS BRANCH
+    # WILL BE BLOCKED!
+    contexts = [ 
+      "run_check"
+    ]
+    strict = true # ensures pull requests targeting a matching branch have been tested with the latest code
+  }
 }
+#####################################################################
 
-resource "github_branch_protection" "custom_protection" {
-  depends_on = [github_repository_collaborators.repo_collaborators]
-  for_each = local.custom_branch_protection_rules
-  repository_id = data.github_repository.repo_info[each.value.repo_name].node_id
-  pattern       = each.value.branch_name
-  allows_deletions = true
-  allows_force_pushes = true
-  
-}
+#################### MODERATE LEVEL BRANCH PROTECTION RULE ##########
+resource "github_branch_protection" "moderate_protection" {
+  depends_on = [github_repository_collaborators.repo_collaborators] # DO NOT CHANGE!
+  for_each = local.custom_branch_protection_rules                 # DO NOT CHANGE!
+  repository_id = data.github_repository.repo_info[each.value.repo_name].node_id # DO NOT CHANGE!
+  pattern       = each.value.branch_name # DO NOT CHANGE!
+  allows_deletions = false # allow users with write permission to delete branch
+  allows_force_pushes = false # allow force pushes to the branch
+  enforce_admins = true # do not allow repository admins to bypass this rule
+  force_push_bypassers = [] # list of collaborators who can bypass force push restriction
+  lock_branch = false # make branch read only
+  require_conversation_resolution = true # all conversations on code must be resolved before a pull request can be merged
+  require_signed_commits = true # commits pushed to matching branches must have verified signatures
+  required_linear_history  = false # prevent merge commits from being pushed
+  required_pull_request_reviews {
+    dismiss_stale_reviews  = true # new reviewable commits pushed to a matching branch will dismiss pull request review approvals
+    pull_request_bypassers = [] # teams/users/apps who are allowed to bypass required pull requests
+    require_code_owner_reviews = true # require an approved review in pull requests including files with a designated code owner
+    require_last_push_approval = true # whether the most recent reviewable push must be approved by someone other than the person who pushed it
+    # number of approvals needed DO NOT CHANGE!
+    required_approving_review_count = lookup(each.value, "required_approving_review_count", 1)
+    restrict_dismissals = false 
+    # teams/users/apps allowed to dismiss pull request reviews 
+    dismissal_restrictions = []
+  } 
 
-resource "github_repository_collaborators" "repo_collaborators" {
-  for_each = local.combined_collaborators
-
-  repository = each.key
-
-  dynamic "user" {
-    for_each = each.value.users
-    content {
-      username   = user.value.username
-      permission = user.value.permission
-    }
+  restrict_pushes {
+  blocks_creations = true # only people, teams, or apps allowed to push will be able to create new branches matching this rule
+  # teams that can push to these branch. DO NOT CHANGE 
+  push_allowances = each.value.push_restrictions == "" ? [] : [data.github_team.team_id[each.value.push_restrictions].node_id]
   }
 
+}
+#####################################################################
+
+################## COLLABORATORS SETTINGS. DO NOT CHANGE! #############
+resource "github_repository_collaborators" "repo_collaborators" {
+  for_each = local.combined_collaborators
+  repository = each.key
   dynamic "team" {
     for_each = each.value.teams
     content {
@@ -105,3 +140,4 @@ resource "github_repository_collaborators" "repo_collaborators" {
     }
   }
 }
+######################################################################
