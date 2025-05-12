c9c9ce80cda14e5e8749b79297a16cd96adbb838
aberforth4 <155059882+aberforth4@users.noreply.github.com>
2025-05-12 18:18:55 +0400

    Update data.tf

--- data.tf
+++ data.tf
@@ -10,6 +10,11 @@ data "github_branch_protection_rules" "branch_protection_rules" {
 }
 
 data "github_team" "team_id" {
-  for_each = local.team_list_set
+  for_each = merge(local.team_list_set, local.default_team_list_set) 
+  slug     = each.key
+}
+
+data "github_app" "app_id" {
+  for_each = merge(local.app_list_set, local.default_app_list_set)
   slug     = each.key
 }
