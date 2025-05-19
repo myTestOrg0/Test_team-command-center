140b65ab5d971284ab0237072b8031ec916b2b49
aberforth4 <155059882+aberforth4@users.noreply.github.com>
2025-05-19 13:38:33 +0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -43,9 +43,18 @@ variable "branch_protection" {
       repo_name = "NewRepoFor", 
       branch_name = "develop", 
       push_teams = ["test_team", "review_dismissals"],
-      push_apps = [  ],
+      push_apps = [],
       review_dismissals = "review_dismissals",
       protection_type = "high", 
+      "required_approving_review_count" = 4
+    },
+{
+      repo_name = "TestRepo", 
+      branch_name = "develop", 
+      push_teams = ["test_team", "review_dismissals"],
+      push_apps = ["emply-github-app"],
+      review_dismissals = "review_dismissals",
+      protection_type = "moderate", 
       "required_approving_review_count" = 4
     }
   ]
@@ -61,7 +70,7 @@ variable "default_branch_protection" {
   })
   default = {
     push_teams = ["test_team"], 
-    push_apps = ["emply-github-app"]
+    push_apps = []
     required_approving_review_count = 4
   } 
 }
