013d585caf1acd4d6e0da8dc52898479eccc9d64
aberforth4 <155059882+aberforth4@users.noreply.github.com>
2025-05-21 14:15:10 +0400

    Delete spyglass directory

--- spyglass/compl_status.py
+++
@@ -1,6 +0,0 @@
-class ComplianceStatus:
-    """Class describes repository policy compliance status"""
-    repository_name = ""
-    status = ""
-    problems_2_fix = []
-    comments = []
--- spyglass/constants.py
+++
@@ -1,4 +0,0 @@
-org_name = "myTestOrg0"
-repositories = ["TestRepo", "NewRepoFor"]
-client_id = "Iv23li6O3dX5Ip5aXYHp"
-installation_id = 63770249
